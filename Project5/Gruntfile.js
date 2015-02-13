c737c31dcde1fccd22640d9cd7338f339349f9cb
Justin Cunningham <justin@bulletprooftiger.com>
2015-02-12 21:54:35 -0800

    Working code

--- Project5/Gruntfile.js
+++ Project5/Gruntfile.js
@@ -1,32 +1,56 @@
 module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-css');
+    grunt.loadNpmTasks('grunt-usemin');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
             all: ['Gruntfile.js', 'src/**/*.js']
         },
-        uglify: {
-            production: {
-                files: {
-                    'dist/output.min.js': ['bower_components/jquery/dist/jquery.js', 'bower_components/knockout/dist/knockout.js', 'src/app.js']
-                }
-            }
-        },
         csslint: {
             production: {
-                src: "css/main.css"
+                src: 'css/main.css'
             }
         },
-        cssmin: {
+        copy: {
             production: {
-                src: 'css/main.css',
-                dest: 'dist/output.min.css'
+                src: 'index.html',
+                dest: '.tmp/index.html'
             }
+        },
+        useminPrepare: {
+            html: 'index.html'
+        },
+        usemin: {
+            html: '.tmp/index.html'
+        },
+        htmlmin: {
+            production: {
+                options: {
+                  removeComments: true,
+                  collapseWhitespace: true
+                },
+                files: {
+                    'dist/index.html': '.tmp/index.html'
+                }
+           }
         }
     });
 
-    grunt.registerTask('default', ['jshint', 'csslint', 'uglify', 'cssmin']);
+    grunt.registerTask('default', [
+        'jshint',
+        'csslint',
+        'copy',
+        'useminPrepare',
+        'concat:generated',
+        'uglify:generated',
+        'cssmin:generated',
+        'usemin',
+        'htmlmin'
+    ]);
 };
---
+++ Project5/js/app.js
@@ -0,0 +1,143 @@
+/* Models */
+// Represents a location
+var Location = function(name, latitude, longitude) {
+    this.name = ko.observable(name);
+    this.latitude = ko.observable(latitude);
+    this.longitude = ko.observable(longitude);
+    this.coordinates = ko.computed(function() {
+        return {lat: this.latitude(), lng: this.longitude()};
+    }, this);
+};
+
+// Represents a business loaded from FourSquare
+var Business = function(fourSquareVenue) {
+    // Compose the business using location, so it can be extended when necessary
+    this.location = new Location(
+        fourSquareVenue.name,
+        fourSquareVenue.location.lat,
+        fourSquareVenue.location.lng
+    );
+};
+
+// Four Square API Helper
+var FourSquare = {
+    clientId: 'RJKS3DSU1ZO1V1M24AECTTITWHS1ZKYM2Y1C3LIQC4XZ5UVJ',
+    clientSecret: 'ONNLRTXE2WPRC1DTH1KGZ34QGM1HRRMMQJPNYBCUZSTNE2VZ',
+    version: '20130815',
+    limit: 50,
+    radius: 250,  // Meters from the location
+    categoryId: '4d4b7105d754a06374d81259',  // Food Category
+    // Searches for food businesses near location.  If a searchTerm is supplied
+    // that term is used in the search.
+    search: function(location, searchTerm) {
+        var data = {
+            client_id: this.clientId,
+            client_secret: this.clientSecret,
+            v: this.version,
+            ll: location.latitude() + "," + location.longitude(),
+            categoryId: this.categoryId,
+            limit: this.limit,
+            radius: this.radius
+        };
+        if (searchTerm && searchTerm.trim().length > 0) data.query = searchTerm;
+        return $.ajax({
+            url: 'https://api.foursquare.com/v2/venues/search',
+            data: data
+        });
+    }
+};
+
+
+/* View Models */
+var ViewModel = function() {
+    var self = this;
+
+    this.googleMap = new google.maps.Map(document.getElementsByClassName('map')[0]);
+
+    this.fourSquareRequest = null;
+
+    // Only update after searchTerm hasn't changed for at least 500ms
+    this.searchTerm = ko.observable().extend(
+            {rateLimit: {timeout: 500, method: "notifyWhenChangesStop"}}
+    );
+    this.searchTerm.subscribe(function(newValue) {
+        this.search(newValue);
+    }, this);
+
+    this.locations = ko.observableArray([
+        new Location('San Carlos', 37.506240, -122.260910),
+        new Location('San Francisco', 37.786863, -122.399789)
+    ]);
+
+    this.businesses = ko.observableArray([]);
+    this.mapMarkers = [];
+
+    this.currentLocation = ko.observable();
+    this.currentLocation.subscribe(function(newValue) {
+        console.log("Switched to city: " + newValue.name());
+
+        // Set the location on the map
+        this.googleMap.setCenter(newValue.coordinates());
+        this.googleMap.setZoom(18);
+
+        // clear the current search
+        $('.searchInput').val('');
+
+        // Update the businesses on the map
+        this.search();
+    }, this);
+
+    this.search = function(searchTerm) {
+        this.resetBusinesses();
+        if (this.fourSquareRequest) this.fourSquareRequest.abort();
+        this.fourSquareRequest = FourSquare.search(this.currentLocation(), searchTerm);
+        this.fourSquareRequest.done(function(json) {
+            json.response.venues.forEach(function(fourSquareVenue) {
+                self.addBusiness(new Business(fourSquareVenue));
+            });
+        }).fail(function() {
+            alert("FourSquare data load failed.  Please reload the page.");
+        });
+    };
+
+    this.resetBusinesses = function() {
+        this.businesses.removeAll();
+
+        // clear map markers
+        this.mapMarkers.forEach(function(marker) {
+            marker.setMap(null);
+        });
+        this.mapMarkers = [];
+    };
+
+    this.addBusiness = function(business) {
+        var marker = new google.maps.Marker({
+            position: business.location.coordinates(),
+            map: this.googleMap,
+            title: business.location.name()
+        });
+        google.maps.event.addListener(marker, 'click', function() {
+            self.selectBusiness(business);
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+            setTimeout(function() { marker.setAnimation(null) }, 700);
+        });
+        this.mapMarkers.push(marker);
+        this.businesses.push(business);
+    };
+
+    this.selectBusiness = function(business) {
+        console.log("Selected " + business.location.name());
+
+        var panorama = self.googleMap.getStreetView();
+        panorama.setPosition(business.location.coordinates());
+        panorama.setVisible(true);
+    };
+
+    // Setup Data
+    this.currentLocation(this.locations()[0]);
+};
+
+$(function () {
+    // Wait for the dom to be loaded to apply the binding
+    ko.applyBindings(new ViewModel());
+});
